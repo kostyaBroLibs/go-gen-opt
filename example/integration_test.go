aa2a3cdb925ec7c417db89e3b2231a32a1799ed1
kostyaBro <indianor201203@gmail.com>
2021-08-01 12:20:53 +0500

    feat: add integration test example

--- example/integration_test.go
+++ example/integration_test.go
@@ -14,3 +14,71 @@
 
 // Package example stores the integrations tests for showing how programm work.
 package example_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Mock for future code
+// #TODO update this later
+type Worker interface {
+	Work(orig []byte) ([]byte, error)
+}
+
+type defaultWorker struct{}
+
+func (dw *defaultWorker) Work(orig []byte) ([]byte, error) {
+	return orig, nil
+}
+
+var tempWorker = &defaultWorker{}
+
+// Mock end
+
+const testCasesFolder = "test_cases"
+
+type integrationTest struct {
+	suite.Suite
+}
+
+func (t *integrationTest) TestFull() {
+	origFileName := "it_original_file.go"
+	expectFileName := "it_expected_file.go"
+	testcases := t.mustProvideTestCases()
+
+	actual, err := tempWorker.Work(testcases[origFileName])
+	t.Require().Nil(err)
+	t.Assert().Equal(testcases[expectFileName], actual)
+}
+
+func TestFull(t *testing.T) {
+	suite.Run(t, new(integrationTest))
+}
+
+func (t *integrationTest) mustProvideTestCases() map[string][]byte {
+	pwd, err := os.Getwd()
+	t.Require().Nil(err, "can't load pwd")
+
+	testCasesFullPath := fmt.Sprintf("%s/%s", pwd, testCasesFolder)
+	dir, err := os.Open(testCasesFullPath)
+	t.Require().Nil(err, "can't open testcases dir")
+
+	files, err := dir.ReadDir(-1)
+	t.Require().Nil(err, "can't read files in tesctace folder")
+
+	output := make(map[string][]byte, len(files))
+	for _, file := range files {
+		fileFullPath := fmt.Sprintf("%s/%s", testCasesFullPath, file.Name())
+		fileBytes, err := ioutil.ReadFile(fileFullPath)
+		t.Require().Nil(err)
+
+		output[file.Name()] = fileBytes
+	}
+
+	return output
+}
